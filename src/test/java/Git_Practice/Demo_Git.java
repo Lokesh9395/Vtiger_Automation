5393dfbaa38d516049171bd1764b1194861270c2
Lokesh <Lokesh@DESKTOP-H9255BB>
2022-01-24 17:09:28 +0530

    user first time commit

---
+++ src/test/java/Git_Practice/Demo_Git.java
@@ -0,0 +1,13 @@
+package Git_Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo_Git 
+{
+	@Test
+	public void sample1_git()
+	{
+		System.out.println("Hi, sharing the project for 1st time");
+	}
+
+}
